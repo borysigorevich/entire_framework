f3a4a7d9e6d33cbfa2efed80720a07c0ab65644f
borysigorevich <borysigorevich@gmail.com>
2023-03-27 21:42:45 +0300

    feature: handle empty query as Homepage

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,13 +8,17 @@ type ItemProps = {
 export const Item = ({title}: ItemProps) => {
     const params = useParams()
 
+    const query = params['label']
+
+    console.log(query)
+
     return (
         <div
             className={`
-            ${params['label'] === title ? '0' : 'pt-[7px] px-3 pb-[9px]'} bg-black text-white text-center transition-all duration-[1s] 
-            overflow-hidden min-w-[206px] [transition-timing-function:ease]`}
+            ${query === title || (!query && title === 'Homepage') ? '0' : 'pt-[7px] px-3 pb-[9px]'} bg-black text-white 
+            text-center transition-all duration-[1s] overflow-hidden min-w-[206px] [transition-timing-function:ease]`}
             style={{
-                maxHeight: params['label'] === title ? 0 : '40px',
+                maxHeight: query === title || (!query && title === 'Homepage') ? 0 : '40px',
             }}
         >
             <Link to={'/' + title}>{title}</Link>
